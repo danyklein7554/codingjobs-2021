f52232a3bddd7b2f1e25185da4ec897bc1c5bca5
Simon <fctinstean@gmail.com>
2021-03-29 15:12:42 +0200

    Forms

--- PHP/Day_2-Date_Forms/Example files/form.php
+++ PHP/Day_2-Date_Forms/Example files/form.php
@@ -31,15 +31,24 @@
 
     //var_dump($_GET);
 
+    // Initialize empty variables (to place in the form)
+    $firstName = '';
+    $lastName = '';
+
     // Make sure I clicked the button
     if (!empty($_GET)) {
+        // Remember the firstname (to place in the form)
+        $firstName = $_GET['firstName'];
+
         echo 'Welcome, ' . $_GET['firstName'] . ' ' . $_GET['lastName'];
     }
 
     ?>
 
     <form action="" method="GET">
-        <input type="text" name="firstName" placeholder="Give your first name">
+
+        <!-- Remembered data goes inside 'value' -->
+        <input type="text" name="firstName" placeholder="Give your first name" value="<?php echo $firstName ?>">
         <br>
 
         <input type="text" name="lastName" placeholder="Give your last name">
