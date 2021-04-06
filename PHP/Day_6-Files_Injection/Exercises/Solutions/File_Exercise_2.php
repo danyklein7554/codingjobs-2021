30885d6906d7d6b6a4a159eda48b1ace99ca2a78
Simon <fctinstean@gmail.com>
2021-04-06 15:28:09 +0200

    Revision project

--- PHP/Day_6-Files_Injection/Exercises/Solutions/File_Exercise_2.php
+++ PHP/Day_6-Files_Injection/Exercises/Solutions/File_Exercise_2.php
@@ -1,6 +1,6 @@
 <?php
 
-	/*
+/*
 		1. Read the file : 'users.txt'
 
 		2. Retrieve, in PHP, all the users as an array.
@@ -12,10 +12,31 @@
 
 		[
 			'username' => 'toto', 
-		'mail' => 'toto@gmail.com'
+			'mail' => 'toto@gmail.com'
 		]
-		
-		
 		
 		*/
 
+if (file_exists('users.txt')) {
+	// Initialize users array
+	$users = array();
+
+	// Open the file
+	$file_handle = fopen('users.txt', 'r');
+
+	// Loop through each line
+	while (!feof($file_handle)) {
+		// Get the current line (string)
+		$currentLine = fgets($file_handle);
+
+		// Explode the array to grab username (position 0) and email(position 1)
+		$currentUser = explode(';', $currentLine);
+
+		echo '<pre>';
+		var_dump($currentUser);
+		echo '</pre>';
+	}
+	echo '<pre>';
+	var_dump($users);
+	echo '</pre>';
+}
