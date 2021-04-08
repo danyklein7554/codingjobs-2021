0bf810ab2918fa3cc6001dd64ef6e2a096bfc30d
Simon <fctinstean@gmail.com>
2021-04-08 14:07:40 +0200

    revision exercise

--- PHP/Revision Exercises and Projects/Revision_Exercise_1.php
+++ PHP/Revision Exercises and Projects/Revision_Exercise_1.php
@@ -1,5 +1,5 @@
 <?php
-
+// faefazef
 /*
 
     Exercise 1 :
@@ -13,3 +13,26 @@
         * If the number is divisible by 5 : display 'Buzz'
         * If the number is divisible by 3 AND 5 : display 'FizzBuzz'
         * Else : display the number 'n'
+
+
+    $array = [20, 5, 74, 32, 50];
+
+*/
+
+$myArray = [5, 15, 6, 7];
+FizzBuzz($myArray);
+
+
+function FizzBuzz($array)
+{
+    foreach ($array as $number) {
+        if (($number % 3 == 0) && ($number % 5 == 0))
+            echo 'FizzBuzz<br>';
+        elseif ($number % 3 == 0)
+            echo 'Fizz<br>';
+        elseif ($number % 5 == 0)
+            echo 'Buzz<br>';
+        else
+            echo $number . '<br>';
+    }
+}
