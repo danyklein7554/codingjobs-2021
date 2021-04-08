e43a1f2bbfe568b7b32fdc0328f6b23bee3a5a26
Simon <fctinstean@gmail.com>
2021-04-08 13:28:59 +0200

    revision exercise

---
+++ PHP/Revision Exercises and Projects/Revision_Exercise_1.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+    Exercise 1 :
+
+    We have an array of integers, positive, between 1 and $nbElements
+    $nbElements is the number of elements.
+
+    Write a function called 'FizzBuzz'.
+    For each number 'n' of the list, we want the following operations :
+        * If the number is divisible by 3 : display 'Fizz'
+        * If the number is divisible by 5 : display 'Buzz'
+        * If the number is divisible by 3 AND 5 : display 'FizzBuzz'
+        * Else : display the number 'n'
