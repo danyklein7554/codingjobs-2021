950e58a359fc696201c0e7bccbf8d549d3fd9c5e
Simon <fctinstean@gmail.com>
2021-04-27 14:22:42 +0200

    PDO Exercise

---
+++ PHP - OOP/Revision Exercises/OOP_Revision_Exercise_3.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+You work for a real estate agent. As part of the redesign of the website, you must
+create a Housing class which will have the following private properties :
+- Title (string 3 to 80 characters)
+- Area (int)
+- City (string 3 to 10 characters)
+- Type (string : house ou appartement)
+- Price (int)
+
+
+Create the getters/setters to validate the type of data above as well as the constructor to instantiate the class.
+
+Add a getInfos() method allowing to return all the properties in the form of an array.
+In a new file, instantiate the class in order to be able to display 3 different Housing objects
+and display the result using the previously created getInfos () method.
+
+*/
