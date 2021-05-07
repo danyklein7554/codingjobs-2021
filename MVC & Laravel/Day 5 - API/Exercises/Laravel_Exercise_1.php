e29583e98d8e6a90e15fde50fc65d5fe2540b477
Simon <fctinstean@gmail.com>
2021-05-07 10:38:31 +0200

    JSON

---
+++ MVC & Laravel/Day 5 - API/Exercises/Laravel_Exercise_1.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/*
+
+    API Exercise
+
+    Based on the flower exercise.
+
+    Step 0:
+        Edit your database.
+        Flower table should now have a 'type' columns (ENUM). Type can be : Magnoliophyta OR Asteraceae 
+
+
+    Step 1: 
+        Create an ApiController
+
+    Step 2:
+        Create routes for :
+            - Get all the flowers
+            - Get only a specific amount of flowers (10 flowers, 25 flowers...)
+            - Get a specific flower based on id
+            - Get all flowers based on a specific type (Magnoliophyta for example)
+
+    Step 3 :
+        Create methods in your controller to match your routes.
+*/
