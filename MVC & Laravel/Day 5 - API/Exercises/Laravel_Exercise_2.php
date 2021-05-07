f9321cea326f8e6b5f9c18daacde436a3f6bdcd7
Simon <fctinstean@gmail.com>
2021-05-07 14:11:37 +0200

    Project Template

--- MVC & Laravel/Day 5 - API/Exercises/Laravel_Exercise_2.php
+++ MVC & Laravel/Day 5 - API/Exercises/Laravel_Exercise_2.php
@@ -12,5 +12,27 @@
         You have to edit FlowerController and views.
     
     Step 2 :
+        Create :
+            - a route '/flower/{id}'
+            - edit the 'show' method of your controller
+            - create a view 'flower-details.blade.php'
 
-        
+        This should display the details of a specific flower.
+    
+    Step 3 :
+
+    Mutatorsexercise
+    
+    Step 4 :
+
+        Now, the page 'flower-details' should also display comments !
+
+        To do this, you have to :
+            1. Create the Comment model
+            2. Edit your Flower and Comment models and implements relationship !
+            3. Now, when you get flowers->all(), it also should grab the associated comments.
+
+        Check the documentation here : https://laravel.com/docs/8.x/eloquent-relationships#one-to-many
+
+
+
